66c3f70e49dd598347964db5b1b34c2b3bc80f6f
Igor <124167777+igormouzzz@users.noreply.github.com>
2024-09-19 21:02:51 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 
 with open('out.txt', 'r') as f:
     x,y = list(map(float, f.readline().strip().split())), list(map(float, f.readline().strip().split()))
+    x2,y2 = list(map(float, f.readline().strip().split())), list(map(float, f.readline().strip().split()))
 x_real = np.linspace(0,1,100)
 #y_real = [xx + exp(-xx) for xx in x_real]
 y_real = [1 + 4/9*xx for xx in x_real]
+#y_real = [xx for xx in x_real]
 plt.plot(x_real, y_real, label = 'Реальное решение',color = 'green')
-plt.scatter(x,y, color = 'red')
+#plt.scatter(x,y, color = 'red')
+#plt.scatter(x,y2, color = 'red')
 plt.plot(x,y, label = 'Численное решение', color = 'blue')
+plt.plot(x2,y2, label = 'Численное решение 2', color = 'purple')
 
 plt.title('Графики функций')  # Заголовок графика
 plt.xlabel('X')  # Подпись оси X
